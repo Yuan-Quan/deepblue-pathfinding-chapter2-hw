b5bddb47e5b34d3341c8a704043952bb566ff00f
Luna Child <metroyuan@gmail.com>
2022-01-19 18:35:52 +0800

    added gitignore

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/cirno/source/repos/deep_blue_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/cirno/source/repos/deep_blue_ws/devel;/opt/ros/noetic'.split(';') if '/home/cirno/source/repos/deep_blue_ws/devel;/opt/ros/noetic' != '' else []
